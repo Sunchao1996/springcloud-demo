7e069367c1eea40490cc49f6ccf0cb59b891825f
sunchao <2061857679@qq.com>
2018-02-06 15:28:43 +0800

    init

--- demo002/src/test/java/com/springcloud/Demo002ApplicationTests.java
+++ demo002/src/test/java/com/springcloud/Demo002ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot;
+package com.springcloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Demo001ApplicationTests {
+public class Demo002ApplicationTests {
 
 	@Test
 	public void contextLoads() {
