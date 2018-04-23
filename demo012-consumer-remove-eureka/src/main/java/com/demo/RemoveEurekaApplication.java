ab30e185e5898c7a7c7ddf5436b4826635336bf4
Sunchao <2061857679@qq.com>
2018-04-23 23:37:24 +0800

    ribbon点对点连接

---
+++ demo012-consumer-remove-eureka/src/main/java/com/demo/RemoveEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoveEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoveEurekaApplication.class, args);
+	}
+}
